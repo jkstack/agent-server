c2e6f1893af45e9f89c296de7f4385a3b22486c8
lwch <lwch748@gmail.com>
2022-09-01 17:58:25 +0800

    增加agent的埋点

--- internal/api/agents/handler.go
+++ internal/api/agents/handler.go
@@ -9,9 +9,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jkstack/anet"
 	"github.com/jkstack/jkframe/stat"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Handler struct {
+	stAgentVersion *prometheus.GaugeVec
+	stAgentInfo    *prometheus.GaugeVec
 }
 
 func New() *Handler {
@@ -23,6 +26,8 @@ func (h *Handler) Module() string {
 }
 
 func (h *Handler) Init(cfg *conf.Configure, mgr *stat.Mgr) {
+	h.stAgentVersion = mgr.RawVec("agent_version", []string{"id", "agent_type", "version", "go_version"})
+	h.stAgentInfo = mgr.RawVec("agent_info", []string{"id", "agent_type", "tag"})
 }
 
 func (h *Handler) HandleFuncs() map[api.Route]func(*gin.Context) {
@@ -38,5 +43,9 @@ func (h *Handler) OnConnect(*agent.Agent) {
 func (h *Handler) OnClose(string) {
 }
 
-func (h *Handler) OnMessage(*agent.Agent, *anet.Msg) {
+func (h *Handler) OnMessage(cli *agent.Agent, msg *anet.Msg) {
+	if msg.Type != anet.TypeAgentInfo {
+		return
+	}
+	h.handleReport(cli.ID(), cli.Type(), msg)
 }
---
+++ internal/api/agents/report.go
@@ -0,0 +1,40 @@
+package agents
+
+import (
+	"github.com/jkstack/anet"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func setValue(vec *prometheus.GaugeVec, id, t, tag string, n float64) {
+	vec.With(prometheus.Labels{
+		"id":         id,
+		"agent_type": t,
+		"tag":        tag,
+	}).Set(n)
+}
+
+func (h *Handler) handleReport(id, t string, msg *anet.Msg) {
+	info := msg.AgentInfo
+	h.stAgentVersion.With(prometheus.Labels{
+		"id":         id,
+		"agent_type": t,
+		"version":    info.Version,
+		"go_version": info.GoVersion,
+	}).Set(1)
+	setValue(h.stAgentInfo, id, t, "cpu_usage", float64(info.CpuUsage))
+	setValue(h.stAgentInfo, id, t, "memory_usage", float64(info.MemoryUsage))
+	setValue(h.stAgentInfo, id, t, "threads", float64(info.Threads))
+	setValue(h.stAgentInfo, id, t, "routines", float64(info.Routines))
+	setValue(h.stAgentInfo, id, t, "startup", float64(info.Startup))
+	setValue(h.stAgentInfo, id, t, "heap_in_use", float64(info.HeapInuse))
+	setValue(h.stAgentInfo, id, t, "gc_0", info.GC["0"])
+	setValue(h.stAgentInfo, id, t, "gc_0.25", info.GC["25"])
+	setValue(h.stAgentInfo, id, t, "gc_0.5", info.GC["50"])
+	setValue(h.stAgentInfo, id, t, "gc_0.75", info.GC["75"])
+	setValue(h.stAgentInfo, id, t, "gc_1", info.GC["100"])
+	setValue(h.stAgentInfo, id, t, "in_packets", float64(info.InPackets))
+	setValue(h.stAgentInfo, id, t, "in_bytes", float64(info.InBytes))
+	setValue(h.stAgentInfo, id, t, "out_packets", float64(info.OutPackets))
+	setValue(h.stAgentInfo, id, t, "out_bytes", float64(info.OutBytes))
+	setValue(h.stAgentInfo, id, t, "reconnect_count", float64(info.ReconnectCount))
+}
